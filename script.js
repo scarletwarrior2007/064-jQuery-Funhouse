2cc74db649816976474e5a6dc0ca638e83b2c894
scarletwarrior2007 <126829250+scarletwarrior2007@users.noreply.github.com>
2023-03-16 17:13:59 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+$(".garage-door-opener").click(function() {
+$(".garage-door").slideToggle();
+});
+
+$(".pokeball").click(function() {
+ $(".pikachu").fadeToggle(); 
+ $(".pokeball").toggle();
+
+});
+
+
+$(".light-switch").click(function() {
+$(".light-bulb").toggle();
+});
+
+$(".ichigo").click(function() {
+$(".bankai").toggle();
+});
